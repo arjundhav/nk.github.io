5448144c8985f5ef052bf7e3f57ffc4207ec4dd2
prasanna4466 <73434401+prasanna4466@users.noreply.github.com>
2021-05-16 17:56:25 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -126,34 +126,28 @@ if(isset($_POST['username'])){
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-4 col-sm-offset-1">
-					<div class="login-form"><!--login form-->
-						<h2>Login to your account</h2>
-						<form action="#" autocomplete="off">
-							<input type="text" name="username" placeholder="Username" />
-							<input type="email" name="password" placeholder="Password" />
-							<span>
-								<input type="checkbox" class="checkbox"> 
-								Keep me signed in
-							</span>
-							<button type="submit" class="btn btn-default">Login</button>
+					<div class="signup-form"><!--sign up form-->
+						<h2>New User Signup!</h2>
+						<form action="signup.php" autocomplete="off" method="post">
+							<input type="text" name="username" placeholder="Username" required/>
+							<input type="email" name="emailid" placeholder="Email Address" required/>
+							<input type="password" name="password" placeholder="Password" required/>
+							<input type="password" name="cpassword" placeholder="Confirm Password" required/>
+
+							<button type="submit" class="btn btn-default">Signup</button>
 						</form>
-					</div><!--/login form-->
+					</div><!--/sign up form-->
 				</div>
 				<div class="col-sm-1">
 					<h2 class="or">OR</h2>
 				</div>
 				<div class="col-sm-4">
-					<div class="signup-form"><!--sign up form-->
-						<h2>New User Signup!</h2>
-						<form action="signup.php" autocomplete="off" method="post">
-							<input type="text" name="username" placeholder="Username"/>
-							<input type="email" name="emailid" placeholder="Email Address"/>
-							<input type="password" name="password" placeholder="Password"/>
-							<input type="password" name="cpassword" placeholder="Confirm Password"/>
-
-							<button type="submit" class="btn btn-default">Signup</button>
+					<div class="signup-form"><!--login form-->
+						<h2>Already have an Account?Log-in</h2>
+						<form action="login.php" autocomplete="off" method="post">
+							<button type="submit" class="btn btn-default">Login</button>
 						</form>
-					</div><!--/sign up form-->
+					</div><!--/login form-->
 				</div>
 			</div>
 		</div>
